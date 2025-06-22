edb612b830710b8dea325b380794520e1ab7429a
gustavolaismann <gustavo110106@edu.unipar.br>
2025-06-22 16:48:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-function mostrarMais(botao) {
-  const card = botao.closest('.card');
-  const info = card.querySelector('.mais-info');
-  const isOpen = card.classList.contains('mostrar');
 
-  document.querySelectorAll('.card').forEach(c => {
-    c.classList.remove('mostrar');
-    c.querySelector('.mais-info').style.maxHeight = '0';
-    c.querySelector('.mais-info').style.opacity = 0;
-  });
-
-  if (!isOpen) {
-    card.classList.add('mostrar');
-    info.style.maxHeight = info.scrollHeight + 'px';
-    info.style.opacity = 1;
-  }
-}
 
 function mostrarMaisAlunos(botao){
     const info = botao.nextElementSibling;
